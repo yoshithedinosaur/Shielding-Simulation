932a0be8f79c492dd37e901cf843a5c56e85b0c2
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-10 16:05:47 -0600

    Initial Commit

---
+++ Shared/Shielding_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shielding_SimulationApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Shielding_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
