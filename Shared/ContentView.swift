c78693151f756bdc47fdfdfb1d8207e04c50b496
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-10 16:18:29 -0600

    first commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var placeholderString = ""
+    
+    // Set up GUI
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment: .center) {
+            Text("Number of neutrons")
+                .font(.callout)
+                .bold()
+            TextField("# neutrons", text: $placeholderString)
+                .padding()
+        }
+        
+        VStack(alignment: .center) {
+            Text("Energy loss")
+                .font(.callout)
+                .bold()
+            TextField("# loss", text: $placeholderString)
+                .padding()
+        }
+        
+        VStack(alignment: .center) {
+            Text("Number Escaped")
+                .font(.callout)
+                .bold()
+            TextField("# Escaped", text: $placeholderString)
+                .padding()
+        }
+        
+        VStack(alignment: .center) {
+            Text("Number Absorbed")
+                .font(.callout)
+                .bold()
+            TextField("# Absorbed", text: $placeholderString)
+                .padding()
+        }
+        
+        VStack(alignment: .center) {
+            Text("Percent Escaped")
+                .font(.callout)
+                .bold()
+            TextField("# Absorbed", text: $placeholderString)
+                .padding()
+        }
+        
     }
 }
 
